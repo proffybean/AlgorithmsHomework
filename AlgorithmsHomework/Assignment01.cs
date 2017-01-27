b44fc383cabe0d7f2c33b32ea6fe2bab84279753
DarthVader\proffy <proffy@comcast.net>
2017-01-26 20:28:38 -0800

    Adding problem 3

---
+++ AlgorithmsHomework/Assignment01.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AlgorithmsHomework
+{
+    [TestClass]
+    public class Assignment01
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            double[] prices = new double[] { 12.30, 33.01, 101.50, 78.00, 31.0, 5.78, 21.18 };
+
+            double min = prices[0];
+            double max = prices[0];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] > max)
+                    max = prices[i];
+
+                if (prices[i] < min)
+                    min = prices[i];
+            }
+
+            Assert.IsTrue(true);
+        }
+
+        public void firsttest()
+        {
+
+        }
+
+    }
+}
